243a5d61bda59758a8effb1ba920aa0c8116a45a
Nourahussein <noura.hussein@sypron.com>
2019-10-29 23:29:00 +0200

    fixing flask api

--- flask_api.py
+++ flask_api.py
@@ -4,16 +4,14 @@ from elasticsearch import Elasticsearch
 app = Flask(__name__, template_folder='template')
 
 es= Elasticsearch
-doc = {
-    'author': 'kimchy',
-    'text': 'Elasticsearch: cool. bonsai cool.',
-    'timestamp': '19-10-2019',
-}
+
 @app.route('/', methods=["GET", "POST"])
 def index():
     q=  request.form.get("q")
     if q is not None:
-        resp= es.search(index='hotels', doc_type='hotel', body={"query": {"match_all": {}}})
+        resp= es.search(index='hotels',  body={"query": {"match_all": {}}})
+        print('Got %d hits:' %res['hits']['total'])
+
         return render_template("index.html", q=q, respnse= resp)
     return render_template('index.html')
 
